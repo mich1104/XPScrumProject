c0cee482e7a18a5fb01d97f57c4f1f24f01728a3
Michael <michaelgarder@outlook.com>
2015-09-14 08:42:59 +0200

    Added ojdbc7.jar in project for use in Jenkins

--- src/test/java/dk/cphbusiness/xpscrumproject/CSVReaderTest.java
+++ src/test/java/dk/cphbusiness/xpscrumproject/CSVReaderTest.java
@@ -27,51 +27,51 @@ public class CSVReaderTest {
         */
     }
     
-//    @Test
-//    public void testCreateCSVReader(){
-//        
-//        CSVReader reader = new CSVReader();
-//        
-//        //Path should be empty
-//        //The splitter should be "," as default
-//        
-//        assertThat(reader.getPath(), is(""));
-//    }
+    @Test
+    public void testCreateCSVReader(){
+        
+        CSVReader reader = new CSVReader();
+        
+        //Path should be empty
+        //The splitter should be "," as default
+        
+        assertThat(reader.getPath(), is(""));
+    }
     
-//    @Test
-//    public void testLoadStudents() throws Exception{
-//        CSVReaderInterface reader = new CSVReader();
-//        String path = "src/testCSVFile.csv";
-//        
-//        try{
-//            createTestFile(path);
-//        }
-//        catch(IOException ioe){
-//            ioe.printStackTrace();
-//            fail("Failed to create test CSV-file");
-//        }
-//        
-//        List<Student> students;
-//        students = reader.loadStudents(path);
-//        
-//        assertThat(reader.getPath(), is(path));
-//        assertThat(students.size(), is(2));
-//        
-//    }
-//    
-//    @Test(expected = IOException.class)
-//    public void testExceptionIfFileNotFound() throws Exception{
-//        String path = "This path does not exist";
-//        CSVReaderInterface reader = new CSVReader();
-//        reader.loadStudents(path);
-//    }
-//    
-//    public void createTestFile(String path) throws IOException{
-//        String toFile = "name, prio1, prio1.1, prio2, prio2.1;name2, prio1, prio1.1, prio2, prio2.1";
-//        PrintWriter pw = new PrintWriter(path);
-//        pw.print(toFile);
-//        pw.flush();
-//        pw.close();
-//    }
+    @Test
+    public void testLoadStudents() throws Exception{
+        CSVReaderInterface reader = new CSVReader();
+        String path = "src/testCSVFile.csv";
+        
+        try{
+            createTestFile(path);
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+            fail("Failed to create test CSV-file");
+        }
+        
+        List<Student> students;
+        students = reader.loadStudents(path);
+        
+        assertThat(reader.getPath(), is(path));
+        assertThat(students.size(), is(2));
+        
+    }
+    
+    @Test(expected = IOException.class)
+    public void testExceptionIfFileNotFound() throws Exception{
+        String path = "This path does not exist";
+        CSVReaderInterface reader = new CSVReader();
+        reader.loadStudents(path);
+    }
+    
+    public void createTestFile(String path) throws IOException{
+        String toFile = "name, prio1.1, prio1.2,prio1,3, prio2.1, prio2.2, prio2.3;name2, prio1.1, prio1.2,prio1,3, prio2.1, prio2.2";
+        PrintWriter pw = new PrintWriter(path);
+        pw.print(toFile);
+        pw.flush();
+        pw.close();
+    }
    
 }
